143ea2bb6b0de65dd676fbebef972897e24553c0
pabmengibar <pabmengibar@gmail.com>
2024-04-16 12:32:35 +0200

    (feature):Adding error msg on api services

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 import { IPokemon, PokemonRes } from '../models/pokemonModel';
 
 @Injectable({
@@ -11,12 +11,19 @@ export class ApiService {
   private_http = inject(HttpClient)
   private urlBase:string = 'https://pokeapi.co/api/v2/pokemon/'
 
-  getPokemonList():Observable<PokemonRes>{
-    return this.private_http.get<PokemonRes>(`${this.urlBase}?limit=151`)
-  }
-  
-  getPokemon(pokemonName:string):Observable<IPokemon>{
-    return this.private_http.get<IPokemon>(`${this.urlBase}${pokemonName}`)
+  getPokemonList(): Observable<PokemonRes> {
+    return this.private_http.get<PokemonRes>(`${this.urlBase}?limit=151`).pipe(
+      catchError(error => {
+        throw 'Error in pokemon list: ' + error;
+      })
+    );
   }
 
+  getPokemon(pokemonName: string): Observable<IPokemon> {
+    return this.private_http.get<IPokemon>(`${this.urlBase}${pokemonName}`).pipe(
+      catchError(error => {
+        throw 'Error in pokemon details: ' + error;
+      })
+    );
+  }
 }
